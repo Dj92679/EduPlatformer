0b5eaaa175e888ce3948b87dc0ffccc105bad053
Dj92679 <dhailes021@gmail.com>
2022-08-05 00:13:06 +1200

    working on enemy scripts

---
+++ EduPlat/Assets/Scripts/Possum AI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PossumAI : MonoBehaviour
+{
+    //reference to waypoints
+    public List<Transform> points;
+    //Int Value for next point index
+    public int nextID;
+
+    int idChangeValue = 1;
+
+    private void Reset()
+    {
+        GetComponent<Box    
+    }
+
+
+
+}

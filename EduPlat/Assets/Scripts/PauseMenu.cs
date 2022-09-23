ff9bbfa6d8c2a34442b43d91968fac7e744c2c78
TwelveAtNight <pc.dylan.su@gmail.com>
2022-09-23 16:20:23 +1200

    Added pause menu to every completed level

---
+++ EduPlat/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void loadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Resume();
+    }
+}
